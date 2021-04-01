b0e3537e75f610a2f7f2821a0c7f658b98631e87
Sidoupiar <sidoupiar@qq.com>
2021-04-01 22:01:48 +0800

    优化代码

--- data.lua
+++ data.lua
@@ -12,4 +12,7 @@ needlist( "zprototype/biology" , "slime" , "fairy" , "tiny" )
 SIGen.NewGroup( "carbon-coke" )
 needlist( "zprototype/carbon_coke" , "1_resource" )
 
+SIGen.NewGroup( "paint" )
+needlist( "zprototype/paint" , "color_block" )
+
 SIGen.Finish()
