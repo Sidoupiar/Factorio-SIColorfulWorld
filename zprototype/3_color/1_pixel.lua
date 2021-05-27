26ec08d91e95e3591c7589f4e921664117c04bcd
Sidoupiar <sidoupiar@qq.com>
2021-05-27 16:41:01 +0800

    优化代码

--- zprototype/3_color/1_pixel.lua
+++ zprototype/3_color/1_pixel.lua
@@ -8,7 +8,7 @@ SIGen.NewSubGroup( "像素块" )
 -- ------- 创建像素调色盘 -------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------------------------
 
-local painterItem = SIGen.NewItem( "像素块调色盘" , 1 )
+SIGen.NewItem( "像素块调色盘" , 1 )
 .AddFlags{ SIFlags.itemFlags.notStackable }
 .SetCustomData
 {
@@ -21,13 +21,12 @@ local painterItem = SIGen.NewItem( "像素块调色盘" , 1 )
 	alt_selection_mode = { "any-entity" } ,
 	alt_selection_cursor_box_type = "copy"
 }
-.GetCurrentEntityName()
 
 -- ------------------------------------------------------------------------------------------------
 -- --------- 创建像素块 ---------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------------------------
 
-local pixelPanelSource = SIGen.NewItem( "像素块母板" , 100 ).SetResults( "sicw-simple-像素块-256-256-256" ).GetCurrentEntityName()
+SIGen.NewItem( "像素块母板" , 100 ).SetResults( "像素块-256-256-256" )
 for r = 0 , SICW.colorMax , SICW.colorStep do
 	for g = 0 , SICW.colorMax , SICW.colorStep do
 		for b = 0 , SICW.colorMax , SICW.colorStep do
@@ -37,7 +36,7 @@ for r = 0 , SICW.colorMax , SICW.colorStep do
 			.SetLocalisedNames{ "entity-name.sicw-simple-像素块" , r , g , b }
 			.SetLocalisedDescriptions{ "entity-description.sicw-simple-像素块" , r , g , b }
 			.SetProperties( 1 , 1 , 10 )
-			.SetMinable( { mining_time = 0.05 , result = pixelPanelSource } , { item = pixelPanelSource , count = 1 } )
+			.SetMinable( SIPackers.Minable( "像素块母板" , 0.05 ) , SIPackers.PlaceableBy( "像素块母板" ) )
 			.SetCorpse( "small-remnants" , "wall-explosion" )
 			.SetMapColor( tint )
 			.SetPic( "pictures" , SIPics.NewLayer( SICW.picturePath.."entity/像素块/像素块" , 32 , 32 ).Priority( SIPics.priority.veryLow ).Tint( tint ).Get() )
